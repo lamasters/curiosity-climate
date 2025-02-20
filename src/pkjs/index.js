4ea89eb42cbb532006bc70181b39ffaea3315333
lamasters <lamasters@protonmail.com>
2025-02-19 20:34:33 -0500

    Initial commit

---
+++ src/pkjs/index.js
@@ -0,0 +1,33 @@
+Pebble.on("message", function (event) {
+  var message = event.data;
+
+  if (message.fetch) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      var response = JSON.parse(this.responseText);
+      if (response.documents) {
+        var weather = response.documents[0];
+        Pebble.postMessage({
+          weather: {
+            high: weather.max_temp,
+            low: weather.min_temp,
+            pressure: weather.pressure,
+            sunrise: weather.sunrise,
+            sunset: weather.sunset,
+            sol: weather.sol,
+          },
+        });
+      }
+    };
+    xhr.open(
+      "GET",
+      "http://homelab.hippogriff-lime.ts.net/v1/" +
+        "databases/6689a86c002a9fb1b740/" +
+        "collections/6689a86f001b93c155ac/documents" +
+        "?project=65aad3806c956cf09df4" +
+        "&queries[0]=%7B%22method%22:%22limit%22,%22values%22:[1]%7D" +
+        "&queries[1]=%7B%22method%22:%22orderDesc%22,%22attribute%22:%22$createdAt%22%7D"
+    );
+    xhr.send();
+  }
+});
---
+++ src/rocky/index.js
@@ -0,0 +1,127 @@
+var rocky = require("rocky");
+
+var weather = {
+  high: 0,
+  low: 0,
+  pressure: 0,
+  sunrise: "00:00",
+  sunset: "00:00",
+  sol: "0",
+};
+
+var months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+rocky.on("draw", function (event) {
+  var ctx = event.context;
+  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+
+  var w = ctx.canvas.unobstructedWidth;
+  var h = ctx.canvas.unobstructedHeight;
+
+  var d = new Date();
+
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.font = "42px bold Bitham";
+  ctx.fillText(
+    (d.getHours() < 10 ? "0" : "") +
+      d.getHours() +
+      ":" +
+      (d.getMinutes() < 10 ? "0" : "") +
+      d.getMinutes(),
+    w / 2,
+    0,
+    w
+  );
+  ctx.font = "28px Gothic";
+  ctx.fillText(
+    days[d.getDay()] + " " + months[d.getMonth()] + " " + d.getDate(),
+    w / 2,
+    40,
+    w
+  );
+
+  ctx.font = "18px bold Gothic";
+  var offsetX = w / 2;
+  var offsetY = 10;
+  var eclipseOffset = 10;
+  var spacing = 16;
+
+  // Adjust alignment if watchface is round
+  if (rocky.watchInfo.platform == "chalk") {
+    offsetY = 20;
+    eclipseOffset = 5;
+    ctx.fillText("sol: " + weather.sol, offsetX, h - (offsetY + spacing), w);
+    ctx.fillText(
+      "day: " + weather.sunrise + "-" + weather.sunset,
+      offsetX,
+      h - (offsetY + spacing * 3),
+      w
+    );
+  } else {
+    ctx.fillText(
+      "sol: " + weather.sol,
+      offsetX,
+      h - (offsetY + spacing * 3),
+      w
+    );
+    ctx.fillText(
+      "day: " + weather.sunrise + "-" + weather.sunset,
+      offsetX,
+      h - (offsetY + spacing),
+      w
+    );
+  }
+  ctx.fillText(
+    "temp: " + weather.high + "/" + weather.low + " °C",
+    offsetX,
+    h - (offsetY + spacing * 2),
+    w
+  );
+
+  if (d.getHours() <= 12) {
+    var xOffset = d.getHours() * 2 - 12;
+  } else {
+    var xOffset = 12 - (d.getHours() - 12) * 2;
+  }
+  ctx.rockyFillRadial(
+    w / 2 + xOffset,
+    h / 2 + eclipseOffset,
+    0,
+    18,
+    0,
+    2 * Math.PI
+  );
+  ctx.fillStyle = "black";
+  ctx.rockyFillRadial(w / 2, h / 2 + eclipseOffset, 0, 17, 0, 2 * Math.PI);
+});
+
+rocky.on("minutechange", function (event) {
+  rocky.requestDraw();
+});
+
+rocky.on("hourchange", function (event) {
+  rocky.postMessage({ fetch: true });
+});
+
+rocky.on("message", function (event) {
+  var message = event.data;
+  if (message.weather) {
+    weather = message.weather;
+    rocky.requestDraw();
+  }
+});
