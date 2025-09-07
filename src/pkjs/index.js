6a8880bf9385043b0adfeedb6785bd8568eb8edc
lamasters <liam.masters3@gmail.com>
2025-09-07 14:00:15 -0400

    Switch to self hosted backend

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -21,10 +21,10 @@ Pebble.on("message", function (event) {
     };
     xhr.open(
       "GET",
-      "https://cloud.appwrite.io/v1/" +
+      "https://appwrite.liammasters.space/v1/" +
         "databases/6689a86c002a9fb1b740/" +
         "collections/6689a86f001b93c155ac/documents" +
-        "?project=67cc9f770031b2c18340" +
+        "?project=68bdaa450031c4b6594c" +
         "&queries[0]=%7B%22method%22:%22limit%22,%22values%22:[1]%7D" +
         "&queries[1]=%7B%22method%22:%22orderDesc%22,%22attribute%22:%22$createdAt%22%7D"
     );
